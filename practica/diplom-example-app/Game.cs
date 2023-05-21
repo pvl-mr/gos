8dbd7c76834be801768152ca39f4e7731ec43296
pvl-mr <mariyapavlova2001@mail.ru>
2023-05-21 23:03:39 +0400

    add tp on c#

---
+++ practica/diplom-example-app/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Implement.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+
+        public string GameName { get; set; }
+
+        public string MasterName { get; set; }
+
+        public DateTime DateGame { get; set; }
+
+        public List<Player> Players { get; set; }
+    }
+}
---
+++ practica/diplom-example-app/SimpleExampleUsers.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace diplom_example_app
+{
+    partial class SimpleExampleUsers
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.buttonDeserealize = new System.Windows.Forms.Button();
+            this.buttonSerealize = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.numericUpDownAge = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAge)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Пользователи";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(16, 51);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(249, 378);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(669, 305);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(304, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Имя";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(304, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Возраст";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(304, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Пол";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(307, 87);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(450, 22);
+            this.textBoxName.TabIndex = 6;
+            // 
+            // buttonDeserealize
+            // 
+            this.buttonDeserealize.Location = new System.Drawing.Point(307, 303);
+            this.buttonDeserealize.Name = "buttonDeserealize";
+            this.buttonDeserealize.Size = new System.Drawing.Size(121, 23);
+            this.buttonDeserealize.TabIndex = 7;
+            this.buttonDeserealize.Text = "Deserealize";
+            this.buttonDeserealize.UseVisualStyleBackColor = true;
+            this.buttonDeserealize.Click += new System.EventHandler(this.buttonDeserealize_Click);
+            // 
+            // buttonSerealize
+            // 
+            this.buttonSerealize.Location = new System.Drawing.Point(443, 303);
+            this.buttonSerealize.Name = "buttonSerealize";
+            this.buttonSerealize.Size = new System.Drawing.Size(124, 23);
+            this.buttonSerealize.TabIndex = 8;
+            this.buttonSerealize.Text = "Serealize";
+            this.buttonSerealize.UseVisualStyleBackColor = true;
+            this.buttonSerealize.Click += new System.EventHandler(this.buttonSerealize_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "-- Выберите пол --",
+            "Женский",
+            "Мужской"});
+            this.comboBox1.Location = new System.Drawing.Point(307, 186);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(450, 24);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // numericUpDownAge
+            // 
+            this.numericUpDownAge.Location = new System.Drawing.Point(307, 275);
+            this.numericUpDownAge.Name = "numericUpDownAge";
+            this.numericUpDownAge.Size = new System.Drawing.Size(450, 22);
+            this.numericUpDownAge.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDownAge);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.buttonSerealize);
+            this.Controls.Add(this.buttonDeserealize);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAge)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Button buttonDeserealize;
+        private System.Windows.Forms.Button buttonSerealize;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDownAge;
+    }
+}
+
